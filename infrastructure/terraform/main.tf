02adb28a15593d6ec15886c791a535e2fcb56cd6
Velshaun Byers <vellbyers@gmail.com>
2026-06-03 01:55:54 -0500

    fix: apply k8s manifests on first deploy, enable EKS public endpoint

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -39,6 +39,9 @@ module "eks" {
   cluster_name    = "${var.project_name}-cluster"
   cluster_version = "1.32"
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
